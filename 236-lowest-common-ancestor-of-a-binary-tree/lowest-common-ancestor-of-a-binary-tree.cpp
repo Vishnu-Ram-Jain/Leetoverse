3937236ce6e078ccc840672f12aa1a04be8e6ced
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-02-18 23:07:00 +0530

    Time: 14 ms (49.99%) | Memory: 16 MB (45.94%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,36 +9,17 @@
  */
 class Solution {
 public:
-    bool f(TreeNode* root, TreeNode* tarnode, vector<TreeNode*> &ds){
-        if(root==NULL)return false;
+    TreeNode* f(TreeNode*root, TreeNode*p, TreeNode*q){
+        if(root==NULL or root==p or root==q)return root;
 
-        ds.push_back(root);
-        if(root==tarnode)return true;
+        TreeNode* left = f(root->left,p,q);
+        TreeNode* right = f(root->right,p,q);
 
-        bool l = f(root->left,tarnode,ds);
-        bool r = f(root->right,tarnode,ds);
-
-        if(l or r)return true;
-
-        ds.pop_back();
-        return false;
+        if(left==NULL)return right;
+        else if(right==NULL)return left;
+        else return root;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> v1,v2;
-        f(root,p,v1);
-        f(root,q,v2);
-        unordered_map<TreeNode*,bool> mp;
-        // cout<<v1.size();
-        // cout<<v2.size();
-        for(auto &i : v1){
-            mp[i]=1;
-        }
-        TreeNode* ans = NULL;
-        for(auto i : v2){
-            if(mp.find(i) != mp.end()){
-                ans = i;
-            }
-        }
-        return ans;
+        return f(root,p,q);
     }
 };
