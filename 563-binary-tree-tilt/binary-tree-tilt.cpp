861084cdb98020d82420abf1f98e9307002564a0
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-02-20 22:49:53 +0530

    Time: 28 ms (10.89%) | Memory: 22.1 MB (80.49%) - LeetSync

--- 563-binary-tree-tilt/binary-tree-tilt.cpp
+++ 563-binary-tree-tilt/binary-tree-tilt.cpp
@@ -11,19 +11,31 @@
  */
 class Solution {
 public:
+    int totsum(TreeNode* root){
+        if(root==NULL)return 0;
+
+        int leftsum = totsum(root->left);
+        int rightsum = totsum(root->right);
+
+        return root->val + leftsum + rightsum;
+    }
     int f(TreeNode* root, int &sum){
         if(root==NULL)return 0;
+
+        int leftsum = totsum(root->left);
+        int rightsum = totsum(root->right);
+
+
+
+        sum += abs(leftsum-rightsum);
 
         int left = f(root->left,sum);
         int right = f(root->right,sum);
 
-        sum += abs(left-right);
 
-        int ld = 0, rd = 0;
-        // if(root->left!=NULL)ld = root->left->val;
-        // if(root->right!=NULL)rd = root->right->val;
+        return root->val;
+        // return root->val + left + right;
 
-        return root->val + left + right;
     }
     int findTilt(TreeNode* root) {
         int sum = 0;
