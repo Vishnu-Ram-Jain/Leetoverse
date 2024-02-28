41f217b79f8ebca7872a406c6f9401e087879607
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-02-28 23:32:44 +0530

    Time: 64 ms (41.52%) | Memory: 38 MB (29.33%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    vis[i][j] = 1;
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        int delrow[]={0,-1,0,1};
+        int delcol[]={1,0,-1,0};
+        while(!q.empty()){
+            auto it = q.front();
+            int row = it.first.first;
+            int col = it.first.second;
+            int dist = it.second;
+            q.pop();
+            dis[row][col] = dist;
+
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol]){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow,ncol},1+dist});
+                }
+            }
+        }
+
+        return dis;
+
+    }
+};
