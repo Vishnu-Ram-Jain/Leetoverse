3f9a2e713b545ea9a26a6b26806f06e3e78abe68
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-03-24 21:16:29 +0530

    Time: 18 ms (42.64%) | Memory: 17.1 MB (26.96%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,56 +1,44 @@
 class Solution {
 public:
-    vector<pair<int,int>> ad[105];
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        int K=k;
-       vector<pair<int, int>> adj[n];
-        for (auto it : flights)
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
+        vector<pair<int,int>> adj[n+1];
+
+        for (auto it : f)
         {
             adj[it[0]].push_back({it[1], it[2]});
         }
 
-        // Create a queue which stores the node and their distances from the
-        // source in the form of {stops, {node, dist}} with ‘stops’ indicating 
-        // the no. of nodes between src and current node.
-        queue<pair<int, pair<int, int>>> q;
-        
-        q.push({0, {src, 0}});
+        // for(int i=0;i<n;i++){
+        //     adj[f[i][0]].push_back({f[i][1],f[i][2]});
+        // }
 
-        // Distance array to store the updated distances from the source.
-        vector<int> dist(n, 1e9);
+        queue<pair<int,pair<int,int>>> q;
+        // stops,node,dist;
+        q.push({0,{src,0}});
+        vector<int> dist(n,1e9);
         dist[src] = 0;
-
-        // Iterate through the graph using a queue like in Dijkstra with 
-        // popping out the element with min stops first.
-        while (!q.empty())
-        {
-            auto it = q.front();
+        while(!q.empty()){
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int distance = q.front().second.second;
             q.pop();
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
 
-            // We stop the process as soon as the limit for the stops reaches.
-            if (stops > K)
-                continue;
-            for (auto iter : adj[node])
-            {
-                int adjNode = iter.first;
-                int edW = iter.second;
+            if(stops > k)continue;
 
-                // We only update the queue if the new calculated dist is
-                // less than the prev and the stops are also within limits.
-                if (cost + edW < dist[adjNode] && stops <= K)
-                {
-                    dist[adjNode] = cost + edW;
-                    q.push({stops + 1, {adjNode, cost + edW}});
+            for(auto it : adj[node]){
+                int adjnode = it.first;
+                int wt = it.second;
+
+                if(dist[adjnode] > distance + wt && stops <= k){
+                    dist[adjnode] = distance + wt;
+                    q.push({stops+1,{adjnode,distance + wt}});
                 }
             }
         }
-        // If the destination node is unreachable return ‘-1’
-        // else return the calculated dist from src to dst.
-        if (dist[dst] == 1e9)
-            return -1;
+        
+
+        if(dist[dst] == 1e9)return -1;
         return dist[dst];
+
     }
 };
