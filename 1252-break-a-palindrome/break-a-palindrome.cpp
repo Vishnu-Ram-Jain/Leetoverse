355ec7f61f186305689b29777e7c48aa43eeb90a
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-03-26 19:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (45.30%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.cpp
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    bool Palindrome(string &s){
-        int i=0;
-        int j = s.size()-1;
-        while(i<=j){
-            if(s[i] != s[j])return false;
-            i++,j--;
-        }
-        return true;
-    }
     string breakPalindrome(string s) {
-        string orig = s;
-        int n = s.size();
-        unordered_map<string,bool> mp;
-        for(int i=0;i<n;i++){
-            for(char c='a';c<='z';c++){
-                char ch = s[i];
-                s[i] = c;
-                if(Palindrome(s) == 0){
-                    mp[s] = 1;
-                }
-                s[i] = ch;
+        int n = s.size();;
+        if(n==1)
+            return string("");
+        
+        for(int i=0; i<n/2; i++) {
+            if(s[i]!='a') {
+                s[i] = 'a';
+                return s;
             }
         }
-        if(mp.size() == 0)return "";
-        auto it = mp.begin();
-
-        string temp = it->first;
-
-        for(auto it : mp){
-            if(temp > it.first)temp = it.first;
-        }
-        return temp;
+        
+        s[n-1] = 'b';
+        return s;
     }
 };
