52fae138d9c88874dd009f17d3b13edff551e93f
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-03-28 13:20:11 +0530

    Time: 14 ms (34.16%) | Memory: 20.5 MB (25.45%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b){
-        if(a[0] == b[0])return a[1] < b[1];
-        return a[0] <= b[0];
-    }
-    vector<vector<int>> insert(vector<vector<int>>& i, vector<int>& newi) {
-        vector<vector<int>> v;
-        i.push_back(newi);
-        sort(i.begin(),i.end(),cmp);
-        v.push_back(i[0]);
-        int n = i.size();
-
-        for(int j=1;j<n;j++){
-            if(v.back()[1] >= i[j][0]){ // merging req;
-                v.back()[1] = max(v.back()[1],i[j][1]);
-            }
-            else{
-                v.push_back(i[j]);
-            }
+    vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& newi) {
+        int x = newi[0];
+        int y = newi[1];
+        int n = arr.size();
+        int i = 0;
+        vector<vector<int>> ans;
+        while(i < n && arr[i][1] < x){
+            ans.push_back(arr[i++]);
         }
-        return v;
+
+        while(i < n && y >= arr[i][0]){
+            x = min(x,arr[i][0]);
+            y = max(y,arr[i][1]);
+            i++;
+        }
+        ans.push_back({x,y});
+        while(i < n){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        return ans;
     }
 };
