59e021822f053c775c673f086f200239450ea976
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-03-30 00:25:50 +0530

    Time: 102 ms (11.99%) | Memory: 11.6 MB (87.72%) - LeetSync

---
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+vector<pair<int,int>> dir = {{0,-1},{-1,0},{0,1},{1,0}};
+    pair<int,int> f(char &ch){
+        int nrow = -1, ncol = -1;
+        if(ch == 'L'){
+            nrow = dir[0].first;
+            ncol = dir[0].second;
+        }
+        else if(ch == 'U'){
+            nrow = dir[1].first;
+            ncol = dir[1].second;
+        }
+        else if(ch == 'R'){
+            nrow = dir[2].first;
+            ncol = dir[2].second;
+        }
+        else if(ch == 'D'){
+            nrow = dir[3].first;
+            ncol = dir[3].second;
+        }
+
+        return {nrow,ncol};
+
+    }
+    // void dfs(int row, int col, string &s, int &cnt, int ind, int n){
+    //     if(ind >= s.size())return ;
+        // for(int j=ind;j<s.size();j++){
+        //     int nrow = row + f(s[j]).first;
+        //     int ncol = col + f(s[j]).second;
+        //     if(nrow>=0 && ncol>=0 && nrow<n && ncol<n){
+        //         cnt++;
+        //         dfs(nrow,ncol,s,cnt,j+1,n);
+        //     }
+        //     else{
+        //         return ;
+        //     }
+        // }
+    // }
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        
+        // int n = s.size();
+        int sz = s.size();
+        vector<int> v(sz,0);
+
+        // int cnt = 0;
+        for(int i=0;i<sz;i++){
+            int cnt = 0;
+            int row = startPos[0];
+            int col = startPos[1];
+            for(int j=i;j<sz;j++){
+                int nrow = row + f(s[j]).first;
+                int ncol = col + f(s[j]).second;
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<n){
+                    cnt++;
+                    row = nrow;
+                    col = ncol;
+                }
+                else{
+                   break;
+                }
+            }
+            v[i] = cnt;
+        }
+        return v;
+    }
+};
