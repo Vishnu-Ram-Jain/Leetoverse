0bd7ae97ce0b70acf55681cf44e9e17400fe6516
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-03-30 00:27:40 +0530

    Time: 30 ms (44.44%) | Memory: 11.7 MB (67.84%) - LeetSync

--- 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.cpp
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-vector<pair<int,int>> dir = {{0,-1},{-1,0},{0,1},{1,0}};
-    pair<int,int> f(char &ch){
-        int nrow = -1, ncol = -1;
-        if(ch == 'L'){
-            nrow = dir[0].first;
-            ncol = dir[0].second;
-        }
-        else if(ch == 'U'){
-            nrow = dir[1].first;
-            ncol = dir[1].second;
-        }
-        else if(ch == 'R'){
-            nrow = dir[2].first;
-            ncol = dir[2].second;
-        }
-        else if(ch == 'D'){
-            nrow = dir[3].first;
-            ncol = dir[3].second;
-        }
-
-        return {nrow,ncol};
-
-    }
     vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
-        
-        // int n = s.size();
-        int sz = s.size();
-        vector<int> v(sz,0);
-        for(int i=0;i<sz;i++){
-            int cnt = 0;
+
+        vector<int>ans;
+        for(int i=0;i<s.size();i++)
+        {
+            int count = 0;
             int row = startPos[0];
             int col = startPos[1];
-            for(int j=i;j<sz;j++){
-                int nrow = row + f(s[j]).first;
-                int ncol = col + f(s[j]).second;
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<n){
-                    cnt++;
-                    row = nrow;
-                    col = ncol;
-                }
-                else{
-                   break;
-                }
+            for(int j=i;j<s.size();j++)
+            {
+                if(s[j]=='L') col--;
+                if(s[j]=='R') col++;
+                if(s[j]=='D') row++;
+                if(s[j]=='U') row--;
+
+                if(col<0 || row<0 || col>=n || row>=n)
+                    break;
+                else
+                    count++;
             }
-            v[i] = cnt;
+            ans.push_back(count);
+
         }
-        return v;
+        return ans;
     }
 };
