876970b250dff533f82f4d5027312c80611da761
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-02 23:50:25 +0530

    Time: 3 ms (91.66%) | Memory: 8.1 MB (85.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> mp;
-        unordered_map<char,bool> mm;
-
+        // unordered_map<char,char> mp;
+        // unordered_map<char,bool> mm;
+        vector<int> mp(256,0);
+        vector<bool> mm(256,0);
         for(int i=0;i<s.size();i++){
-            if(mp.find(s[i]) != mp.end()){
+            if(mp[s[i]] != 0){
                 if(mp[s[i]] != t[i])return false;
             }
+            // if(mp.find(s[i]) != mp.end()){
+            //     if(mp[s[i]] != t[i])return false;
+            // }
+            // else{
+            //     if(mm.find(t[i]) != mm.end())return false;
+            //     mp[s[i]] = t[i];
+            //     mm[t[i]] = 1;
+            // }
             else{
-                if(mm.find(t[i]) != mm.end())return false;
+                if(mm[t[i]] != 0)return false;
                 mp[s[i]] = t[i];
                 mm[t[i]] = 1;
             }
