6783b3d24687396f580526b907b712e5166eb3c9
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-03 20:14:22 +0530

    Time: 62 ms (72.85%) | Memory: 20.1 MB (42.77%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>> q;
-        unordered_set<string> st(wordList.begin(),wordList.end());
-        st.erase(beginWord);
-        q.push({beginWord,1});
-        while(!q.empty()){
-            string word = q.front().first;
-            int steps = q.front().second;
-            q.pop();
-            if(word == endWord)return steps;
+        unordered_map<string,bool> mp;
+        
+        for(auto i : wordList) mp[i] = 1;
 
-            for(int i=0;i<word.size();i++){
-                char original = word[i];
+        queue<pair<int,string>> q;
+        q.push({1,beginWord});
+
+        while(!q.empty()){
+            string temp = q.front().second;
+            int steps = q.front().first;
+            q.pop();
+            if(temp == endWord)return steps;
+
+            for(int i=0;i<temp.size();i++){
+                char orig = temp[i];
                 for(char ch='a';ch<='z';ch++){
-                    word[i] = ch;
-                    if(st.find(word)!=st.end()){
-                        st.erase(word);
-                        q.push({word,steps+1});
+                    temp[i] = ch;
+                    if(mp.find(temp) != mp.end()){
+                        q.push({1+steps,temp});
+                        mp.erase(temp);
                     }
                 }
-                word[i] = original;
+                temp[i] = orig;
             }
         }
         return 0;
