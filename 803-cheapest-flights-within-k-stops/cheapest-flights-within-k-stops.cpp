e8f60ed8b71a6b2d48f15f90db862b347241a05f
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-07 00:26:40 +0530

    Time: 21 ms (25.65%) | Memory: 17.3 MB (22.62%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n+1];
+    typedef pair<int,pair<int,int>> pp;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+       vector<pair<int,int>> adj[n+1];
 
-        for (auto it : f)
+        for (auto it : flights)
         {
             adj[it[0]].push_back({it[1], it[2]});
         }
 
-        // for(int i=0;i<n;i++){
-        //     adj[f[i][0]].push_back({f[i][1],f[i][2]});
-        // }
-
-        queue<pair<int,pair<int,int>>> q;
-        // stops,node,dist;
-        q.push({0,{src,0}});
         vector<int> dist(n,1e9);
         dist[src] = 0;
-        while(!q.empty()){
-            int stops = q.front().first;
-            int node = q.front().second.first;
-            int distance = q.front().second.second;
-            q.pop();
+
+        priority_queue<pp,vector<pp>,greater<pp>> pq;
+        pq.push({0,{0,src}});
+
+        while(!pq.empty()){
+            int stops = pq.top().first;
+            int wt = pq.top().second.first;
+            int node = pq.top().second.second;
+            pq.pop();
 
             if(stops > k)continue;
 
+
             for(auto it : adj[node]){
                 int adjnode = it.first;
-                int wt = it.second;
+                int edgewt = it.second;
 
-                if(dist[adjnode] > distance + wt && stops <= k){
-                    dist[adjnode] = distance + wt;
-                    q.push({stops+1,{adjnode,distance + wt}});
+                if(dist[adjnode] > wt + edgewt){
+                    dist[adjnode] = wt + edgewt;
+                    pq.push({1+stops,{dist[adjnode],adjnode}});
                 }
+
             }
         }
-        
 
-        if(dist[dst] == 1e9)return -1;
-        return dist[dst];
+        return dist[dst] == 1e9 ? -1 : dist[dst];
 
     }
 };
