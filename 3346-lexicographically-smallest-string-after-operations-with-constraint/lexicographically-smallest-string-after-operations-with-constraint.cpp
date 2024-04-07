bbc04f91fcaba58ac8bd24239034839395eb09e8
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-07 23:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (40.00%) - LeetSync

--- 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.cpp
+++ 3346-lexicographically-smallest-string-after-operations-with-constraint/lexicographically-smallest-string-after-operations-with-constraint.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int dist(string &s, string &t){
-        int dst=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] != t[i]){
-                int fwd = abs(s[i] - t[i]) ;
-                int bwd = abs(26 - (s[i] - t[i]));
-                dst += min(fwd,bwd);
-            }
-        }
-        return dst;
-    }
+    // int dist(string &s, string &t){
+    //     int dst=0;
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i] != t[i]){
+    //             int fwd = abs(s[i] - t[i]) ;
+    //             int bwd = abs(26 - (s[i] - t[i]));
+    //             dst += min(fwd,bwd);
+    //         }
+    //     }
+    //     return dst;
+    // }
     string getSmallestString(string s, int k) {
         int n = s.size();
         string t = s;
         for(int i=0;i<n;i++){
             for(char ch = 'a';ch <= 'z';ch++){
-                t[i] = ch;
-                if(dist(s,t) <= k){
-                    t = min(t,s);
+                int dist = min(s[i]-ch,26-(s[i] - ch));
+                if(dist <= k){
+                    t[i] = ch;
+                    k -= dist;
                     break;
                 }
             }
