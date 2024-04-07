ddcf917457f79553208dffd359d811c2de2e4b60
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-07 19:17:16 +0530

    Time: 3 ms (45.50%) | Memory: 9.3 MB (9.43%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool f(string &s, int open, int i, vector<vector<int>> &dp){
         if(i >= s.size())return open==0;
+        if(open < 0)return false;
         if(dp[i][open] != -1)return dp[i][open];
 
         if(s[i] == '('){
             return dp[i][open] = f(s,open+1,i+1,dp);
         }
         else if(s[i] == ')'){
-            if(open==0) return dp[i][open] = false;
-            else return dp[i][open] = f(s,open-1,i+1,dp);
+            return dp[i][open] = f(s,open-1,i+1,dp);
         }
-        else if(s[i] == '*'){
+        else{
             bool op1=0,op2=0,op3=0;
             op1 = f(s,open+1,i+1,dp);
             if(open)op2 = f(s,open-1,i+1,dp);
@@ -19,7 +19,6 @@ public:
 
             return dp[i][open] = op1 || op2 || op3;
         }
-        return true;
     }
     bool checkValidString(string s) {
         int n = s.size();
