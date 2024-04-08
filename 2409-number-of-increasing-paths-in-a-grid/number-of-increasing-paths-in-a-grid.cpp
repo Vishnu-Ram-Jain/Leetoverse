14af2f887c1e7f3b777223652d5107305f583463
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-08 23:47:42 +0530

    Time: 163 ms (56.52%) | Memory: 46.1 MB (88.26%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int dfs(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp, int n, int m, int prev){
-        if(i<0 or j<0 or i>=n or j>=m or grid[i][j] <= prev)return 0;
+    int delrow[4] = {0,1,0,-1};
+    int delcol[4] = {1,0,-1,0};
+    int dfs(int n, int m, int row, int col, vector<vector<int>> &mat, int prev,vector<vector<int>> &dp){
+        if(mat[row][col] <= prev)return 0;
 
-        if(dp[i][j] != -1)return dp[i][j];
+        if(dp[row][col] != -1)return dp[row][col];
 
-        int a = 1 + dfs(i,j+1,grid,dp,n,m,grid[i][j]);
-        int b = 0 + dfs(i-1,j,grid,dp,n,m,grid[i][j]);
-        int c = 0 + dfs(i,j-1,grid,dp,n,m,grid[i][j]);
-        int d = 0 + dfs(i+1,j,grid,dp,n,m,grid[i][j]);
+        int steps = 1;
 
-        return dp[i][j] = (a+b+c+d) % mod;
+        for(int i=0;i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow>=0 && ncol>=0 && ncol<m && nrow<n){
+                steps += dfs(n,m,nrow,ncol,mat,mat[row][col],dp);
+            }
+        }
+        return dp[row][col] = steps % mod;
     }
     int countPaths(vector<vector<int>>& grid) {
         int n = grid.size();
@@ -20,7 +27,7 @@ public:
         vector<vector<int>> dp(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans = (ans + dfs(i,j,grid,dp,n,m,-1)) % mod;
+                ans = (ans + dfs(n,m,i,j,grid,-1,dp)) % mod;
             }
         }
         return ans % mod;
