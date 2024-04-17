7d5646bafbb53c0b18458fe2dd5647843197a0d6
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-17 23:31:20 +0530

    Time: 104 ms (49.14%) | Memory: 29.1 MB (65.98%) - LeetSync

---
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int dp[1001][5000];
+    int f(int pos, int e, int k){
+        if(k == 0){
+            if(pos == e)return 1;
+            return 0;
+        }
+        if(dp[k][pos+1000] != -1)return dp[k][pos+1000];
+        int op2 = f(pos-1,e,k-1);
+        int op1 = f(pos+1,e,k-1);
+
+        return dp[k][pos+1000] = ((op1 % mod) + (op2 % mod)) % mod;
+
+    }
+    int numberOfWays(int startPos, int endPos, int k) {
+        memset(dp,-1,sizeof(dp));
+        return f(startPos,endPos,k);
+    }
+};
