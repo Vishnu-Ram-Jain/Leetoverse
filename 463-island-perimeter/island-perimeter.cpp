9da78ee53086c4b890ff02495ac65c4231c9714a
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-04-18 21:45:12 +0530

    Time: 70 ms (69.58%) | Memory: 104.3 MB (13.16%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -3,7 +3,9 @@ public:
         int delrow[4] = {0,1,0,-1};
         int delcol[4] = {1,0,-1,0};
     int dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis,int n, int m){
+        
         if(row>=n or col>=m or row<0 or col<0 or grid[row][col] == 0)return 1;
+        
         if( vis[row][col] == 1)return 0;
         vis[row][col] = 1;
         int cnt = 0;
@@ -24,7 +26,7 @@ public:
         int per = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1 &&!vis[i][j]){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     per = dfs(i,j,grid,vis,n,m);
                     break;
                 }
