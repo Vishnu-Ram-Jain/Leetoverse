672848a75150b0e02e5e3fbd14c506c27962442e
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-05-22 12:22:25 +0530

    Time: 7 ms (48.14%) | Memory: 16 MB (48.05%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         int low = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
         int high = upper_bound(nums.begin(),nums.end(),target) - nums.begin();
-        cout<<low<<high<<endl;
+        // cout<<low<<high<<endl;
         if(low < n && high <= n && nums[low] == target && nums[high-1] == target)return {low,high-1};
-        cout<<low<<high<<endl;
+        // cout<<low<<high<<endl;
 
         return {-1,-1};
     }
