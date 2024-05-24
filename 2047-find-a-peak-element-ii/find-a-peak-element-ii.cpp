2292a6eaf11e3be8546ad3ef2970195da7e9952f
V2002ishnu <107766981+V2002ishnu@users.noreply.github.com>
2024-05-24 19:05:47 +0530

    Time: 123 ms (64.47%) | Memory: 48.9 MB (81.45%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    int maxi(vector<vector<int>>& mat, int n, int m, int mid){
+        int maxele_ind = -1;
+        int maxi = -1;
+        for(int i=0;i<n;i++){
+            if(mat[i][mid] > maxi){
+                maxi = mat[i][mid];
+                maxele_ind = i; 
+            }
+        }
+        return maxele_ind;
+    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
         int row = -1, col = -1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i>0){
-                    if(mat[i-1][j] >= mat[i][j])continue;
-                }
-                if(i<n-1){
-                    if(mat[i+1][j] >= mat[i][j])continue;
-                }
-                if(j<m-1){
-                    if(mat[i][j+1] >= mat[i][j])continue;
-                }
-                if(j>0){
-                    if(mat[i][j-1] >= mat[i][j])continue;
-                }
-                return {i,j};
-            }
-        } 
+        int s = 0;
+        int e = m-1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            int row = maxi(mat,n,m,mid);
+            int left = mid-1 >= 0 ? mat[row][mid-1] : -1;
+            int right = mid+1 < m ? mat[row][mid+1] : -1;
+
+            if(mat[row][mid] > left && mat[row][mid] > right)return {row,mid};
+            else if(mat[row][mid] < left)e = mid-1;
+            else s = mid+1;
+        }
         return {-1,-1};  
     }
 };
