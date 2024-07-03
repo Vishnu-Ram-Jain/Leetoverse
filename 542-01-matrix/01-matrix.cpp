bec9e4fccb7fe3b36faa70d7aba0014b40756677
Vishnu_Jain <107766981+Vishnu-Ram-Jain@users.noreply.github.com>
2024-07-03 12:45:24 +0530

    Time: 67 ms (17.48%) | Memory: 37.9 MB (37.40%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<pair<int,int>,int>> q;
         int n = mat.size();
         int m = mat[0].size();
-
+        vector<vector<int>> v(n,vector<int>(m,0));
         vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dis(n,vector<int>(m,0));
-
-        queue<pair<pair<int,int>,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -16,28 +14,26 @@ public:
                 }
             }
         }
-        int delrow[]={0,-1,0,1};
-        int delcol[]={1,0,-1,0};
-        while(!q.empty()){
-            auto it = q.front();
-            int row = it.first.first;
-            int col = it.first.second;
-            int dist = it.second;
-            q.pop();
-            // dis[row][col] = dist;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
 
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dist = q.front().second;
+            q.pop();
+            v[row][col] = dist;
             for(int i=0;i<4;i++){
                 int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol]){
+                if(nrow>=0 && ncol>=0 && ncol<m && nrow<n && !vis[nrow][ncol]
+                && mat[nrow][ncol] == 1){
                     vis[nrow][ncol] = 1;
-                    q.push({{nrow,ncol},1+dist});
-                    dis[nrow][ncol] = 1 + dist;
+                    q.push({{nrow,ncol},1+dist});    
                 }
             }
         }
 
-        return dis;
-
+        return v;
     }
 };
