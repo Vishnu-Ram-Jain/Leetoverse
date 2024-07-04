906b480e9a2feb21df8887fce46b90eaa1e896f6
Vishnu_Jain <107766981+Vishnu-Ram-Jain@users.noreply.github.com>
2024-07-04 13:38:17 +0530

    Time: 40 ms (20.04%) | Memory: 20.3 MB (8.80%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     typedef pair<int,int> pp;
-    int f(int n, int src, vector<pair<int,int>> adj[], int k){
+    int f(int n, vector<pair<int,int>> adj[], int k, int src){
         vector<int> dist(n,1e9);
         dist[src] = 0;
-
         priority_queue<pp,vector<pp>,greater<pp>> pq;
         pq.push({0,src});
 
         while(!pq.empty()){
-            int node = pq.top().second;
-            int dis = pq.top().first;
+            auto it = pq.top();
             pq.pop();
-            for(auto it : adj[node]){
-                int adjnode = it.first;
-                int edgewt = it.second;
+            int node = it.second;
+            int dis = it.first;
 
+            for(auto &i : adj[node]){
+                int adjnode = i.first;
+                int edgewt = i.second;
                 if(dis + edgewt < dist[adjnode]){
                     dist[adjnode] = dis + edgewt;
                     pq.push({dist[adjnode],adjnode});
                 }
             }
         }
+
         int cnt = 0;
+
         for(int i=0;i<n;i++){
             if(dist[i] != 1e9 && dist[i] <= k)cnt++;
         }
@@ -37,13 +39,14 @@ public:
             adj[u].push_back({v,wt});
             adj[v].push_back({u,wt});
         }
-        int mincnt = 1e9;
-        int city = -1;
+
+        int city = n;
+        int temp = n;
         for(int i=0;i<n;i++){
-            int cnt = f(n,i,adj,distanceThreshold);
-            if(mincnt >= cnt){
-                mincnt = cnt;
+            int cnt = f(n,adj,distanceThreshold,i);
+            if(cnt <= temp){
                 city = i;
+                temp = cnt;
             }
         }
         return city;
