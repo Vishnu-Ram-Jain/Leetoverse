3e7c890df2ee5c79fda656ffa5425ae07e782a06
Vishnu_Jain <107766981+Vishnu-Ram-Jain@users.noreply.github.com>
2024-07-10 20:13:33 +0530

    Time: 9 ms (22.38%) | Memory: 14 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int cnt = 0;
+        for(auto &it : logs){
+            string s = it;
+            if(s == "../"){
+                if(cnt > 0)cnt--;
+            }
+            else if(s == "./"){
+
+            }
+            else{
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
