456b3d6c93efbe4c058d143518294335b595dde4
Vishnu_Jain <107766981+Vishnu-Ram-Jain@users.noreply.github.com>
2024-07-11 13:58:32 +0530

    Time: 260 ms (31.52%) | Memory: 294.9 MB (5.99%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int dp[2501][2501];
-    int f(int i, int prev_i, vector<int> &nums){
-        if(i >= nums.size())return 0;
+    // int dp[2501][2501];
+    // int f(int i, int prev_i, vector<int> &nums){
+    //     if(i >= nums.size())return 0;
 
-        if(dp[i][prev_i+1] != -1)return dp[i][prev_i+1];
+    //     if(dp[i][prev_i+1] != -1)return dp[i][prev_i+1];
         
-        int pick = 0;
-        if(prev_i == -1 or nums[i] > nums[prev_i]){
-            pick = 1 + f(i+1,i,nums);
-        }
-        int np = f(i+1,prev_i,nums);
+    //     int pick = 0;
+    //     if(prev_i == -1 or nums[i] > nums[prev_i]){
+    //         pick = 1 + f(i+1,i,nums);
+    //     }
+    //     int np = f(i+1,prev_i,nums);
 
-        return dp[i][prev_i+1] = max(pick,np);
-    }
+    //     return dp[i][prev_i+1] = max(pick,np);
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return f(0,-1,nums);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                int pick = 0;
+                if(prev == -1 or nums[i] > nums[prev]){
+                    pick = 1 + dp[i+1][i+1];
+                }
+                int np = dp[i+1][prev+1];
+                dp[i][prev+1] = max(pick,np);
+            }
+        }
+        return dp[0][0];
     }
 };
