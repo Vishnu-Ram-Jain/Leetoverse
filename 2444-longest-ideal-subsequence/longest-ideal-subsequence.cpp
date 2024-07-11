2e7baee60906c997e0e5d4f0e548184fcf98339b
Vishnu_Jain <107766981+Vishnu-Ram-Jain@users.noreply.github.com>
2024-07-11 17:53:46 +0530

    Time: 302 ms (33.26%) | Memory: 37.1 MB (31.56%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int dp[100001][31];
+    bool check(int a, int b, int k){
+        return abs(b-a) <= k;
+    }
+    int dp[100001][40];
     int f(int i, int prev, string &s, int k){
         if(i >= s.size())return 0;
-        if(dp[i][prev] != -1)return dp[i][prev];
 
-        int pick = 0, notpick = 0;
+        if(dp[i][prev+1] != -1)return dp[i][prev+1];
 
-        int curr = s[i] - 'a' + 1;
-        if(prev == 0 or abs(curr - prev) <= k){
-            pick = 1 + f(i+1,curr,s,k);
+        int pick = 0;
+        if(prev == -1 or check(s[i]-'a',prev,k)){
+            pick = 1 + f(i+1,s[i]-'a',s,k);
         }
-        notpick = f(i+1,prev,s,k);
+        int np = f(i+1,prev,s,k);
 
-        return dp[i][prev] = max(pick,notpick);
+        return dp[i][prev+1] = max(pick,np); 
     }
     int longestIdealString(string s, int k) {
         memset(dp,-1,sizeof(dp));
-        return f(0,0,s,k);
+        return f(0,-1,s,k);
     }
 };
